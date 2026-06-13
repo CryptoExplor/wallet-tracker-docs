df79e92d299224f77880c3c9d4b89342a00dc4ca
Ravi <102873939+CryptoExplor@users.noreply.github.com>
2026-06-14 01:09:07 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
+        guide: 'guide.html',
         privacy: 'privacy.html',
         terms: 'terms.html',
         support: 'support.html'
